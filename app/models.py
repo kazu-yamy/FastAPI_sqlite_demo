83ff0337dff38d815b51e543573931e3362b5b39
Macha_Found <kazuki.aishima.dev@gmail.com>
2022-12-05 12:40:23 +0900

    create test model

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, Float, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    user_name = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    
+    items = relationship("Item", back_populates="owner")
+
+class Item(Base):
+    __tablename__ = "items"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(String, index=True)
+    time = Column(String, index=True)
+    device_id = Column(Integer, index=True)
+    temp = Column(Float)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    
+    owner = relationship("User", back_populates="items")
+    
