d153f2f197f4f3c59f5a304677f51d3b6cbec36b
macha <kazuki.aishima@gmail.com>
2022-12-07 07:14:43 +0900

    save

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ def get_db():
         db.close()
 
 
-@app.get("/users/", response_model=schemas.User)
+@app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_name(db=db, user_name=user.user_name)
     if db_user:
@@ -32,7 +32,7 @@ def read_users(ski: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 @app.get("/users/{user_id}")
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
-    if db_user in None:
+    if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
